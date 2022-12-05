482b2d0ce9624fb430d4e75988996ea9d402fc70
Sym3ioZ <tortevois.benoit@gmail.com>
2022-12-05 18:35:33 +0100

    tests for http requests.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  ngOnInit(): void {}
+  movie!: object;
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http
+      .get<object>(
+        'https://api.themoviedb.org/3/movie/829280?api_key=032445021a055e1fc596f3292981c16d&language=fr-FR'
+      )
+      .subscribe((movie) => (this.movie = movie));
+    console.log(this.movie);
+  }
 }
