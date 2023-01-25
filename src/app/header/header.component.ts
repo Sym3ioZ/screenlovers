938c44c6ddab403700c0de397b23be14915d25e0
Sym3ioZ <tortevois.benoit@gmail.com>
2023-01-25 11:04:09 +0100

    Responsive full OK. Slight adjusts. Ready to deploy, V0.99.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -21,28 +21,36 @@ export class HeaderComponent implements OnInit {
   // On clicking ? link
   onDiscover() {
     window.scrollTo({ top: 0 });
-    this.toggleMenu();
+    if (window.innerWidth < 768) {
+      this.toggleMenu();
+    }
     this.router.navigateByUrl('');
   }
 
   // On clicking Home link
   onHomePage() {
     window.scrollTo({ top: 0 });
-    this.toggleMenu();
+    if (window.innerWidth < 768) {
+      this.toggleMenu();
+    }
     this.router.navigateByUrl('home');
   }
 
   // On clicking search link
   onSearch(): void {
     window.scrollTo({ top: 0 });
-    this.toggleMenu();
+    if (window.innerWidth < 768) {
+      this.toggleMenu();
+    }
     this.router.navigateByUrl('search');
   }
 
   // On clicking contact link
   onContact(): void {
     window.scrollTo({ top: 0 });
-    this.toggleMenu();
+    if (window.innerWidth < 768) {
+      this.toggleMenu();
+    }
     this.router.navigateByUrl('contact');
   }
 
@@ -58,7 +66,6 @@ export class HeaderComponent implements OnInit {
       bar2!.style.top = '42.5%';
       arrows!.style.top = '-125%';
       document.body.style.overflowY = '';
-      // document.body.style.paddingRight = '';
       header!.style.transform = 'translateY(-100vh) translateX(-100vw)';
       window.setTimeout(() => {
         this.menuToggle = !this.menuToggle;
@@ -72,9 +79,6 @@ export class HeaderComponent implements OnInit {
       bar2!.style.top = '85%';
       arrows!.style.top = '0';
       document.body.style.overflowY = 'hidden';
-      // if (+document.body.offsetHeight > window.innerHeight) {
-      //   document.body.style.paddingRight = '10px';
-      // }
 
       this.menuToggle = !this.menuToggle;
       window.setTimeout(() => {
